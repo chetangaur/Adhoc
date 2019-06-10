187684618a4a6d43ddf9217bcd75b4481d0cc7c9
chetangaur <43693378+chetangaur@users.noreply.github.com>
2019-06-10 09:46:58 +0530

    Solution of Problem1
    To take input from a user about their name and age and tell them, after how many years they become 95 years old.

---
+++ Problem1.py
@@ -0,0 +1,11 @@
+>>> print("Enter your name and Age")
+Enter your name and Age
+>>> name=input()
+Chetan
+>>> age=input()
+21
+>>> x=95-int(age)
+>>> print(x)
+74
+>>> print(x,"years after age is 95")
+74 years after age is 95
