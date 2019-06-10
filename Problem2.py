3be0be2e2a56b21c43032bac4836944eb221c5f8
chetangaur <43693378+chetangaur@users.noreply.github.com>
2019-06-10 11:48:34 +0530

    Solution2.py
    Basic operation on  list

---
+++ Problem2.py
@@ -0,0 +1,17 @@
+adhoc=[1,2,3,1,4,5,66,22,2,6,0,9]
+a=[]
+b=[]
+for i in adhoc:
+    if i>5:
+        print(i)
+        a.append(i)
+    elif i<=2:
+        print(i)
+        b.append(i)
+print(a)
+print(b)
+        
+
+
+
+        
