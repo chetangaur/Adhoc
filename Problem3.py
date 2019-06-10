614d9f1b9f6dffbf4ca73ff1eb1f3bf061a017b2
chetangaur <43693378+chetangaur@users.noreply.github.com>
2019-06-10 14:40:23 +0530

    Solution3.py
    This python code helps you to access the present time on the clock and wish you greetings according to present time.

---
+++ Problem3.py
@@ -0,0 +1,11 @@
+import datetime
+input()
+h=datetime.datetime.now().hour
+if(4<h<12):
+    print("Good morning")
+elif(12<18):
+    print("Good afternoon")
+elif(18-21):
+    print("Good evening")
+elif(21-24):
+    print("Good night")
